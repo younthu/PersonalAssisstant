10aace42576db6f618e1f52a90a3867a432743b3
Andrew Yang <evil_eve_live@outlook.com>
2022-11-18 18:22:49 +0800

    antd 5 add float button

--- src/pages/Newtab/index.jsx
+++ src/pages/Newtab/index.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import { Provider } from "react-redux";
 import { createStore } from 'redux';
 import rootReducer from './todos/reducers'
+import { FloatButton } from 'antd';
 import { persistStore, persistReducer } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
@@ -19,7 +20,12 @@ let persistor = persistStore(store)
 
 // used localStorage for redux persist, if want to remove the persist, please check it here: https://github.com/rt2zz/redux-persist .
 render(<Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}><TodoApp /></PersistGate>
+        <PersistGate loading={null} persistor={persistor}>
+        <div>
+        <TodoApp />
+        <FloatButton onClick={() => console.log('click')} />
+        </div>
+        </PersistGate>
 </Provider>, window.document.querySelector('#app-container'));
 
 if (module.hot) module.hot.accept();
